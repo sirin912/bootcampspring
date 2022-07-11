adc19406fdb471a542ae5259a3c5eb2a496376db
sirin912 <cyrineouni123@gmail.com>
2022-07-11 13:55:20 +0100

    passage de data vers la vue

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -1,14 +1,16 @@
 package com.sip.ams.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	@RequestMapping("/info")
-	
-	public String info() {
+	public String info(Model model) {
+		String formation="Fullstack";
 		System.out.println("methode info");
+		model.addAttribute("workshop",formation);
 		return "home/info";
 	}
 	@RequestMapping("/affiche")
